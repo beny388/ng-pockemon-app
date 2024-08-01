5b5b2e1c62f67e2523daa6847689cc9306a9ec7f
Mohamed Beny <mohamedabdoulayebenycoulibaly@gmail.com>
2024-08-01 14:37:32 +0100

    conditionner un affichage avec ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 import { POKEMONS } from './mock-pokemon-list';
 import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl:'app.component.html'
+  imports: [RouterOutlet, CommonModule],  // Added CommonModule here
+  templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
+  pokemonSelected: Pokemon | undefined;
 
   ngOnInit() {
     console.table(this.pokemonList);
   }
 
   selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon=>pokemon.id== +pokemonId);
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
 
-    if(pokemon){
-    console.log(`vous avez cliqué sur le pokemon ${pokemon.name}`);
-    this.pokemonSelected= pokemon;
-
-    } else{
+    if (pokemon) {
+      console.log(`vous avez cliqué sur le pokemon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
       console.log(`vous avez demandé un pokemon qui n'existe pas`);
-    this.pokemonSelected= pokemon;
+      this.pokemonSelected = pokemon;
     }
   }
 }
